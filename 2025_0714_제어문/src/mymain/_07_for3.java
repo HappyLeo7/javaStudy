f4b92cb0cc9b052c01fbfbd694df047d6b2579df
HappyLeo7 <suh0007@naver.com>
2025-07-14 18:19:42 +0900

    for 유형3

--- 2025_0714_제어문/src/mymain/_07_for3.java
+++ 2025_0714_제어문/src/mymain/_07_for3.java
@@ -60,24 +60,57 @@ public class _07_for3 {
 		int n = 0;
 		for (int i = 'A'; i <= 'Z'; i++) { // i = 65
 
+//			//방법1)
+//			System.out.printf("%c",i);
+//			if(i=='Z') {
+//				break;
+//			}else
+//			if(i%3==2) {
+//				System.out.printf("%c",'-');
+//			}else
+//			if(i%3==1) {
+//				System.out.printf("%c",'~');
+//			}else
+//			if(i%3==0) {
+//				System.out.printf("%c",'=');
+//			}
+
+			// 방법2)
 			System.out.printf("%c", i);
-			if (n%3==0) {
-				System.out.printf("%c", '-');				
-			}  
-			if (n%3==2) {
-				System.out.printf("%c", '~');				
-			}  
-			if (n%3==1) {
-				System.out.printf("%c", '=');				
-			}  
+			if (i == 'Z') {
+				break;
+			}
+			if (n % 3 == 0) {
+				System.out.printf("%c", '-');
+			}
+			if (n % 3 == 2) {
+				System.out.printf("%c", '~');
+			}
+			if (n % 3 == 1) {
+				System.out.printf("%c", '=');
+			}
 			n = n + 1;
 
-//			System.out.printf("%c",'-');
-//			System.out.print(i);
-//			System.out.print(n);
-			
 		}
 		System.out.println();// 줄바꾸기
+
+		// AZBY...
+		//방법1)
+//		int m=25;
+		for (int i = 'A', m = 25,o=1; i <= 'Z'; i++, m--,o=o-5) {
+
+			System.out.printf("%c", i);
+			System.out.printf("%c", 65 + m);
+//			System.out.printf("%c%c",i,65+m);
+			
+			
+		}
+		System.out.println();
+		
+		//방법2)
+		for (int i = 0; i < 26; i++) {
+			System.out.printf("%c%c", 'A' + i, 'Z' - i);
+		}
 
 	}// main_end
 }
