01fddefec36721919b71a8bcc93e00562c8a291d
HappyLeo7 <suh0007@naver.com>
2025-07-25 18:34:24 +0900

    배열

--- 2025_0723_String/src/mymain/MyMain_05_문제_자판기.java
+++ 2025_0723_String/src/mymain/MyMain_05_문제_자판기.java
@@ -14,23 +14,25 @@ public class MyMain_05_문제_자판기 {
 		String y_n="y";
 		
 		
-		System.out.println("금액 : ");
+		System.out.print("금액 : ");
 		money = scanner.nextInt();
 		VendingMachine_자판기 vendingMachine = new VendingMachine_자판기();
 		while(true) {
 			
-			System.out.println("[음료선택]\n1.콜라(2000)\n2.사이다(1800)\n3.쥬스(3000)\n4.우유(1400)\n ");
+			System.out.print("[음료선택]\n1.콜라(2000)\n2.사이다(1800)\n3.쥬스(3000)\n4.우유(1400)\n ");
 			drink=scanner.nextInt();
 			if(0>=drink||drink>=5) {
 				System.out.println("존재하지않는 음료입니다.");
-				continue;
+				break;
 			}
 			//VendingMachin에게 금액과 선택음료정보전달
 			vendingMachine.insertCoin(money);
 			vendingMachine.selectDrink(drink);
 			
 			//결과출력
-			vendingMachine.displayResult();
+			vendingMachine.displayResult(); //방법1) 잔돈에서도 계속해서 구매가능
+//			vendingMachine.displayResult2(); //방법2) 1번만 구매되고 다시 돈넣고 실행해야함
+//			vendingMachine.displayResult3(); //방법3) 1번만 구매되고 다시 돈넣고 실행해야함
 			money=vendingMachine.getmoney();
 			
 			System.out.println("계속하시겠습니까? ('y' or 'n')");
--- 2025_0723_String/src/myutil/VendingMachine_자판기.java
+++ 2025_0723_String/src/myutil/VendingMachine_자판기.java
@@ -9,6 +9,9 @@ public class VendingMachine_자판기 {
 	private int money;
 	private int drink;
 	private String drink_name;
+	// 음료이름 /가격 배열= 0 1 2 3 4
+	String[] drink_name2 = { "", "콜라", "사이다", "쥬스", "우유" };
+	int[] drink_price = { 0, COLA, CYDER, JUICE, MILK };
 
 	// 생성자
 	public VendingMachine_자판기() {
@@ -37,32 +40,67 @@ public class VendingMachine_자판기 {
 
 	public void displayResult() {
 		System.out.printf("받은금액 : %d원\n", this.money);
-		if(drink==1&&this.money<COLA||drink==2&&this.money<CYDER||drink==3&&this.money<JUICE||drink==3&&this.money<MILK) {
+		if (this.money < 1400 || drink == 1 && this.money < COLA || drink == 2 && this.money < CYDER
+				|| drink == 3 && this.money < JUICE || drink == 4 && this.money < MILK) {
 			System.out.println("구매 할 수 있는 물품이 없습니다.");
-			System.out.printf("거스름돈 : %d원\n",this.money);
-		}else {
-			
-		switch (drink) {
-		case 1:
-			this.money = this.money - this.COLA;
-			System.out.printf("선택한음료 : %s(%d원)\n거스름돈 : %d원\n", this.drink_name, this.COLA, this.money);
-			break;
-		case 2:
-			this.money = this.money - this.CYDER;
-			System.out.printf("선택한음료 : %s(%d원)\n거스름돈 : %d원\n", this.drink_name, this.CYDER, this.money);
-			break;
-		case 3:
-			this.money = this.money - this.JUICE;
-			System.out.printf("선택한음료 : %s(%d원)\n거스름돈 : %d원\n", this.drink_name, this.JUICE, this.money);
-			break;
-		case 4:
-			this.money = this.money - this.MILK;
-			System.out.printf("선택한음료 : %s(%d원)\n거스름돈 : %d원\n", this.drink_name, this.MILK, this.money);
-			break;
+			System.out.printf("거스름돈 : %d원\n", this.money);
+		} else {
+
+			switch (drink) {
+			case 1:
+				this.money = this.money - this.COLA;
+				System.out.printf("선택한음료 : %s(%d원)\n거스름돈 : %d원\n", this.drink_name, this.COLA, this.money);
+				break;
+			case 2:
+				this.money = this.money - this.CYDER;
+				System.out.printf("선택한음료 : %s(%d원)\n거스름돈 : %d원\n", this.drink_name, this.CYDER, this.money);
+				break;
+			case 3:
+				this.money = this.money - this.JUICE;
+				System.out.printf("선택한음료 : %s(%d원)\n거스름돈 : %d원\n", this.drink_name, this.JUICE, this.money);
+				break;
+			case 4:
+				this.money = this.money - this.MILK;
+				System.out.printf("선택한음료 : %s(%d원)\n거스름돈 : %d원\n", this.drink_name, this.MILK, this.money);
+				break;
+			}
 		}
-		}
-		
 
 	}
+	
+	public void displayResult2() {
+		if(drink_price[drink]>money) {
+			System.out.println("잔액이 부족합니다.");
+			System.out.printf("남은잔액 : %d원\n",this.money);
+			return;
+		}
+		System.out.println("---[결과]---");
+		System.out.printf("받은 금액 : %d\n",money);
+		System.out.printf("선택 음료 : %s(%d)\n",drink_name2[drink],drink_price[drink]);
+		System.out.printf("거스름 돈 : %d\n", money-drink_price[drink]);
+		
+	}
+	
+	public void displayResult3() {
+		String name ="";
+		int price=0;
+		
+		switch(drink) {
+		case 1 : name="콜라";price=COLA;break;
+		case 2 : name="사이다";price=CYDER; break;
+		case 3 : name="쥬스";price=JUICE;break;
+		case 4 : name="우유";price=MILK;break;
+		}
+		if(drink_price[drink]>money) {
+			System.out.println("잔액이 부족합니다.");
+			System.out.printf("남은잔액 : %d원\n",this.money);
+			return;
+		}
+		System.out.println("---[결과]---");
+		System.out.printf("받은 금액 : %d\n",money);
+		System.out.printf("선택 음료 : %s(%d)\n",name,price);
+		System.out.printf("거스름 돈 : %d\n", money-price);
+		
+	}//end display3
 
 }
